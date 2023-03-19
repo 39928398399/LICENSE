d74a85f4ad04bf2856734b963cfbc7c942e12536
39928398399 <128291678+39928398399@users.noreply.github.com>
2023-03-19 17:31:26 +0530

    snake.py
    Hello guys I'm making a snake game under 5 minutes
    so I'm kartik

--- game.py
+++ game.py
@@ -9,6 +9,7 @@ snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
 snake.center = get.random.position()
 length = 1
 segments = [snake.copy()]
+snake_dir = (0, 0)
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
@@ -16,8 +17,21 @@ while True:
   for event in pg.event():
     if event.type == pg.QUIT:
          exit()
-  screen.fill('black')
-  # draw snake(screen, 'green', segment) for segment in segments]
-  [pg.draw.rect
-  pg.display.flip()
-  clock.tick(60)
+    if event.type == pg.KEYDOWN:
+       if event.key == pg.K_w:
+         snake_dir = (0, - TILE_SIZE)
+       if event.key == pg.K_s:
+         snake_dir = (0, - TILE_SIZE)
+       if event.key == pg.K_s:
+         snake_dir = (-TILE_SIZE, 0)
+       if event.key == pg.K_d:
+         snake_dir = (TILE_SIZE, 0)
+     # draw snake
+     [pg.draw.rect(screen, 'green', segment) for segment in segments]
+     # move snake
+     snake.move_ip(snake_dir)
+     segments.append(snake.copy())
+     segments= segments[-length:]
+     screen.fill('black')
+     pg.display.flip()
+     clock.tick(60)
