bfa968e04bd8935e41b3005ddc1bc53a7bbdb648
39928398399 <128291678+39928398399@users.noreply.github.com>
2023-03-19 16:52:35 +0530

    game.py
    this a snake game under 5 minutes
    Credit: kartim

---
+++ game.py
@@ -0,0 +1,23 @@
+import pygame as pg
+from random import randrange
+
+WINDOW = 1000
+TILE_SIZE = 50
+RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+get_random_position = Lambda: [randrange(*RANGE), randrange(*RANGE)]
+snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
+snake.center = get.random.position()
+length = 1
+segments = [snake.copy()]
+screen = pg.display.set_mode([WINDOW] * 2)
+clock = pg.time.Clock()
+
+while True:
+  for event in pg.event():
+    if event.type == pg.QUIT:
+         exit()
+  screen.fill('black')
+  # draw snake(screen, 'green', segment) for segment in segments]
+  [pg.draw.rect
+  pg.display.flip()
+  clock.tick(60)
